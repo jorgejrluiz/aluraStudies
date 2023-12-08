c35290961df3080a57f12311e98719b469b49e96
Jorge Luiz <jorgejrluiz98@gmail.com>
2023-12-08 17:02:50 -0300

    Refactor Button to function component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import style from './button.module.scss'
 
+interface ButtonProps {
+    text : string,
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void
+}
 
-class Button extends React.Component<{
+function Button ({onClick, type, text} : ButtonProps) {
+    return (
+        <button onClick={onClick} type={type} className={style.button}>
+            {text}
+        </button>
+    )
+}
+
+
+class refactoButton extends React.Component<{
     text : string,
     type?: "button" | "submit" | "reset" | undefined,
     onClick?: () => void}> {
     render() {
         const { type = 'button', onClick } = this.props;
-
         return (
             <button onClick={onClick} type={type} className={style.button}>
                 {this.props.text}
